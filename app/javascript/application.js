e68421c697a248b78834205195272012ad173a0e
haripandey21 <iamgoing1214@gmail.com>
2022-09-29 05:10:16 +0800

    web3 integrate

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,4 +4,27 @@ import "controllers"
 import * as bootstrap from "bootstrap"
 
 
+import Web3 from 'web3'
 
+$ (async () => {
+    let accounts
+    let account 
+    let signature=null
+    let message = "testing";
+
+    if (typeof window.ethereum == "undefined") {
+        
+        alert("Please Install Metamask");
+    } else {
+       
+        const web3 = new Web3(await Web3.givenProvider)
+        accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        message = `Your want to Login in this website via account ${accounts[0]}`
+        let signature = await web3.eth.personal.sign(message,accounts[0])
+        console.log("signature : ",signature)
+    }
+    
+})
+
+
+        
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.1/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "web3", to: "https://ga.jspm.io/npm:web3@1.8.0/dist/web3.min.js"
